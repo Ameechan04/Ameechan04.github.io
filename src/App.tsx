662fe3e1f42888a7e5720f39e6c1889091f293a9
Andrew Meechan <andrewmeechan2004@gmail.com>
2026-09-10 14:08:33 +0100

    Added animated background to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
+import FaultyTerminal from "./components/FaultyTerminal"
+
 function App() {
 
   return (
     <>
-      <h1 className="text-5xl"> Testing </h1>
+      <div style={{ width: '100vw', height:'100vh', position: 'relative' }}>
+        <FaultyTerminal
+          scale={1.6}
+          gridMul={[2, 1]}
+          digitSize={1.2}
+          timeScale={0.5}
+          pause={false}
+          scanlineIntensity={0.3}
+          glitchAmount={1}
+          flickerAmount={1}
+          noiseAmp={1}
+          chromaticAberration={0}
+          dither={0}
+          curvature={0}
+          tint="#ffffff"
+          mouseReact
+          mouseStrength={0.8}
+          pageLoadAnimation
+          brightness={0.6}
+        />
+      </div>
     </>
   )
 }
